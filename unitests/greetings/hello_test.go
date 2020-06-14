fddbb135fea75b7f20f641e03ffed1c93aff7167
Davi Kawasaki <davishinjik@gmail.com>
2020-06-14 16:42:39 -0300

    Returning test to older version

--- unitests/greetings/hello_test.go
+++ unitests/greetings/hello_test.go
@@ -2,9 +2,10 @@ package main
 
 import "testing"
 
-// test hello function with empty argument
-func TestHelloEmptyArg(t *testing.T) {
+// test hello function
+func TestHello(t *testing.T) {
 	defaultEmptyMsg := "Hello Dude!"
+	defaultMikeMsg := "Hello Mike!"
 
 	// test for empty argument
 	emptyResult := hello("") // should return "Hello Dude!"
@@ -14,11 +15,6 @@ func TestHelloEmptyArg(t *testing.T) {
 	} else {
 		t.Logf("hello(\"\") SUCCESS, expected %v, got %v", defaultEmptyMsg, emptyResult)
 	}
-}
-
-// test hello function with valid argument
-func TestHelloValidArg(t *testing.T) {
-	defaultMikeMsg := "Hello Mike!"
 
 	// test for valid argument
 	result := hello("Mike") // should return "Hello Mike!"
