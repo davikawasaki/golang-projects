e8648cc6a6d7d333a2fee1de0e8ad95151645085
Davi Kawasaki <davishinjik@gmail.com>
2020-06-14 15:13:32 -0300

    Added simple log for success cases

--- unitests/greetings/hello_test.go
+++ unitests/greetings/hello_test.go
@@ -11,13 +11,17 @@ func TestHello(t *testing.T) {
 	emptyResult := hello("") // should return "Hello Dude!"
 
 	if emptyResult != defaultEmptyMsg {
-		t.Errorf("hello(\"\") failed, expected %v, got %v", defaultEmptyMsg, emptyResult)
+		t.Errorf("hello(\"\") FAILED, expected %v, got %v", defaultEmptyMsg, emptyResult)
+	} else {
+		t.Logf("hello(\"\") SUCCESS, expected %v, got %v", defaultEmptyMsg, emptyResult)
 	}
 
 	// test for valid argument
 	result := hello("Mike") // should return "Hello Mike!"
 
 	if result != defaultMikeMsg {
-		t.Errorf("hello(\"Mike\") failed, expected %v, got %v", defaultMikeMsg, result)
+		t.Errorf("hello(\"Mike\") FAILED, expected %v, got %v", defaultMikeMsg, result)
+	} else {
+		t.Logf("hello(\"Mike\") SUCCESS, expected %v, got %v", defaultMikeMsg, result)
 	}
 }
